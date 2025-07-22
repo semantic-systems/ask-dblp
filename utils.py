a42ad69677d724b32cc76ecb69f362bb97b2c1f1
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-07-22 16:41:01 +0200

    define load_json and write_to_json functions in utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,20 @@
 import json
 
+def load_json_data(file_name):
+    try:
+        with open(file_name, 'r') as json_file:
+            data = json.load(json_file)
+        return data
+    except FileNotFoundError:
+        print(f"Error: File '{file_name}' not found.")
+        return []
+
+def write_to_json(result, out_file_path):
+    with open(out_file_path, "w", encoding="utf-8") as f:
+        json.dump(result, f, indent=4)
+    print("Successfully written to file!")
+
+
 def extruct_values(results):
     return_result = []
     for result in results["results"]["bindings"]:
