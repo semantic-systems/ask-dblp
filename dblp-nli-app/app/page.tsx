fe54bd6e8b523493482b8e8441c87f092ac8825d
Tilahun Abedissa <58974800+huntila@users.noreply.github.com>
2025-08-06 08:58:31 +0200

    remove unused text area from UI

--- dblp-nli-app/app/page.tsx
+++ dblp-nli-app/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import {useEffect, useState} from "react";
 import { Card, CardContent } from "@/components/ui/card";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {toast, Toaster} from "sonner";
